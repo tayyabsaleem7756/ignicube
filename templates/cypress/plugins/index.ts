5a13b821b591610d6a870a3b435458014f5b35c3
Tayyab Saleem <tayyabsaleem7756@gmail.com>
2024-03-19 13:28:24 +0500

    index update

--- templates/cypress/plugins/index.ts
+++ templates/cypress/plugins/index.ts
@@ -2,8 +2,8 @@
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 export default (on, config) => {
-  config = require('@optimumqa/cypress-setup-utilities')(on, config)
-  require('@optimumqa/cypress-store')(on, config)
+  config = require('@tayyabsaleem7756/cypress-setup-utilities')(on, config)
+  require('@tayyabsaleem7756/cypress-store')(on, config)
 
   allureWriter(on, config);
 
