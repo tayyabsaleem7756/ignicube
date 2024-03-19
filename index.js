7ba73cd7e82a4a0ca7071a0c77e83bb7b6e21cbe
Tayyab Saleem <tayyabsaleem7756@gmail.com>
2024-03-19 14:05:12 +0500

    index

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ var copyRecursiveSync = function (src, dest) {
 
 function main() {
   const args = process.argv.slice(2)
-  copyRecursiveSync(path.join(__dirname, 'templates'), args[0] || './optimumqa-cypress-boilerplate/')
+  copyRecursiveSync(path.join(__dirname, 'templates'), args[0] || './optimumqa-cypress-boilerplates/')
   console.log('\nDone.')
   console.log('\nRun the following commands to add your first project:')
-  console.log(`1. cd ${args[0] || './optimumqa-cypress-boilerplate/'}`)
+  console.log(`1. cd ${args[0] || './optimumqa-cypress-boilerplates/'}`)
   console.log('2. npm install')
   console.log('3. npm run add-project')
 }
