196769acf606b936ca8166b518a311fdb9e826b1
Tayyab Saleem <tayyabsaleem7756@gmail.com>
2024-03-19 21:42:43 +0500

    vf

--- templates/_templates/project/with-prompt/prompt.js
+++ templates/_templates/project/with-prompt/prompt.js
@@ -1,4 +1,4 @@
-module.exports = [{
+export default [{
     type: 'input',
     name: 'project',
     message: 'Project name?',
@@ -10,11 +10,27 @@ module.exports = [{
   },
   {
     type: 'confirm',
-    name1: 'configure',
+    name: 'configure',
     message: 'Do you want configure testrail?',
-    if (_name1 = true) {
-      console.log('sample here')
-    }
   },
-
+  {
+    type: 'input',
+    name: 'projectId',
+    message: 'What is the projectId for this project?',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is the username for testrails?',
+  },
+  {
+    type: 'input',
+    name: 'password',
+    message: 'What is the password for testrails?',
+  },
+  {
+    type: 'input',
+    name: 'suiteId',
+    message: 'What is the suiteId for testrails?',
+  },
 ]
