e69993b677eab9ad1943ab06629fbcea6ed55159
Tayyab Saleem <tayyab.saleem@ignicube.com>
2024-04-03 21:27:38 +0500

    test

--- templates/templates/_templates/project/with-prompt/Commands.ejs.t
+++
@@ -1,7 +0,0 @@
----
-to: cypress/support/e2e/<%= project %>/commands.js
----
-
-const commands = {}
-
-export default commands
--- templates/templates/cypress/plugins/index.js
+++
@@ -1,6 +0,0 @@
-const TestRailReporter = require('cypress-testrail');
-
-module.exports = (on, config) => {
-  new TestRailReporter(on, config).register();
-  return config
-}
